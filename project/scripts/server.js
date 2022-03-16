edbae8c2e5e10df945a1abd5987d1cc0fa3112b1
Ayman <ayman.ali1302@gmail.com>
2022-03-16 10:22:19 +0000

    Testing express with index.html

--- project/scripts/server.js
+++ project/scripts/server.js
@@ -1,1 +1,76 @@
 /* File formost of node/express operations */
+
+const express = require('express');
+const { connect } = require('./dbconfig');
+const app = express()
+var path = require('path')
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname);
+
+const sayHi = require('./dbconfig');
+const mysql = require('mysql2')
+var port = process.env.PORT;
+console.log(port)
+
+//5005
+
+app.get('/', (req, res) =>{
+    console.log("WORK")
+    res.send("<h1>Hi</h1>")
+    // res.render('login.html')
+
+});
+app.get('/index', (req, res) => {  
+    console.log("index")
+    // res.writeHead(200, {'content-type':'text/html'})
+    app.use(express.static(path.join(__dirname, 'public')));
+    res.render('index.html');
+    res.end()
+    // console.log('user here')
+    // console.log(sayHi)
+    
+
+    // res.send("i here")
+    // res.end()
+    // sayHi.getConnection((err, connection) => {
+    //     // if(err) throw err
+    //     console.log(`connected as id ${connection.threadID}`)
+    //     connection.query('SELECT * from ProblemType', (err, rows) => {
+    //         connection.release()
+            
+    //         res.send(rows)
+        
+
+    //     })
+    // })
+
+    // const con = require('./dbconfig');
+
+    // con.query("SELECT * from ticket", function (err, result, fields) {
+    //     if (err) throw err;
+    //     console.log(result);
+
+    //     const table = document.querySelector('ticket-body employee-ticket-body');
+    // });
+
+    // con.end();
+
+    // sayHi.query("{SELECT * from ProblemType", function (err, result, fields) {
+    //     // if (err) throw err;
+    //     console.log(result);
+    //     res.end(result)
+    // });
+    
+    // sayHi.end();
+
+
+    //killall -9 node
+    
+});
+// var port = normalizePort(process.env.PORT);
+// app.set('port', port);
+
+
+
+app.listen(port)
