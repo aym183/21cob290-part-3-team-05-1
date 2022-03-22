942a00c3d791c8f3b1cebb1c642132755bdd06b4
James K <jkinasz@hotmail.co.uk>
2022-03-22 17:40:20 +0000

    Updates to login functioanlity

--- project/server.js
+++ project/server.js
@@ -40,6 +40,35 @@ app.get('/login.html', (req, res) =>{
 
 });
 
+app.all('/auth', (req, res) {
+	// Capture the input fields
+    const con = require('./public/scripts/dbconfig');
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		con.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				req.session.loggedin = true;
+				req.session.username = username;
+				// Redirect to home page
+				res.redirect('/home');
+			} else {
+				res.send('Incorrect Username and/or Password!');
+			}			
+			res.end();
+		});
+	} else {
+		response.send('Please enter username and password!');
+		response.end();
+	}
+});
+
 app.get('/index.html', (req, res) => {  
     console.log("index")
     // res.writeHead(200, {'content-type':'text/html'})
