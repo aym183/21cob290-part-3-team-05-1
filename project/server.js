92aae19bf9157234a9a950e75da445a2f509261f
Ayman <ayman.ali1302@gmail.com>
2022-03-27 13:03:55 +0100

    Added queries to get id's of inputs for updating ticket

--- project/server.js
+++ project/server.js
@@ -232,16 +232,24 @@ app.get('/index.html', (req, res) => {
 
         socket.on("update_message", (msg) => {
             console.log(msg);
-            handler_id = msg.handler_name;
-            problem_type_id = msg.problem_type;
-            software_id = msg.software;
-            con.query(`SELECT problem_type_id from problem_type;`,[problem_type_id],function (err, result, fields) {
+            
+            con.query(`SELECT problem_type_id from problem_type where name = ?;`,[msg.problem_type],function (err, result, fields) {
                 if (err) throw err;
-                console.log(result);
-                
-             
-          });
-          con.end();
+                problem_type_id = result[0].problem_type_id;
+            });
+            con.query(`SELECT software_id from software where name = ?;`,[msg.software],function (err, result, fields) {
+                if (err) throw err;
+                software_id = result[0].software_id;
+            });
+            con.query(`SELECT user_id from handler INNER JOIN employee ON employee.employee_id  = handler.user_id WHERE employee.name = ?
+                    UNION
+                    SELECT external_specialist_id AS user_id FROM external_specialist WHERE name = ?`,[msg.handler_name,msg.handler_name],function (err, result, fields) {
+                if (err) throw err;
+                handler_id = result[0].user_id;
+            });
+      con.end();
+
+            
 
         });
     
