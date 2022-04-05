8b4cbcdf8986af76675673b3661cd68e8504190b
LadiOgun <94852827+LadiOgun@users.noreply.github.com>
2022-04-05 21:38:43 +0100

    new attempts to get solution displaying

--- project/public/scripts/faq.js
+++ project/public/scripts/faq.js
@@ -1,5 +1,5 @@
 function addRowHandlers(data) {
-    var table = document.getElementById("tableId");
+    var table = document.getElementById("table-info");
     var rows = table.getElementsByTagName("tr");
     for (i = 0; i < rows.length; i++) {
       var currentRow = table.rows[i];
@@ -7,7 +7,6 @@ function addRowHandlers(data) {
         return function() {
           var cell = row.getElementsByTagName("td")[0];
           var id = cell.innerHTML;
-          alert("id:" + id);
         };
       };
       currentRow.onclick = createClickHandler(currentRow);
--- project/server.js
+++ project/server.js
@@ -99,7 +99,8 @@ app.get('/faq.html', (req, res) =>{
 
             con.query(`SELECT solution.solution_description FROM solution 
             INNER JOIN ticket_solution ON solution.solution_id = ticket_solution.solution_id 
-            INNER JOIN ticket ON ticket_solution.ticket_id = ticket.ticket_id WHERE ticket.ticket_id=?;`,
+            INNER JOIN ticket ON ticket_solution.ticket_id = ticket.ticket_id 
+            WHERE ticket.ticket_id=3 AND solution_status = "successful";`,
             [parseInt(msg.id)],function(err, result, fields) {
             console.log(err);
             if (err) throw err;
