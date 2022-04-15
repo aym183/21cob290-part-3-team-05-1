d8f222e8058be0088531c7da7db95d41b811d341
Ayman <ayman.ali1302@gmail.com>
2022-04-15 01:39:30 +0100

    Added dummy data

--- project/db/dummy_data.sql
+++ project/db/dummy_data.sql
@@ -34,7 +34,8 @@ INSERT INTO `users` VALUES
 (2013 ,'HollyL' , AES_ENCRYPT('Holly_Pw',SHA2("HollyL",256))),
 (2014 ,'Will Taha' , AES_ENCRYPT('Will_Pw',SHA2("Will Taha",256))),
 (2015 ,'AliH' , AES_ENCRYPT('Ali_Pw',SHA2("AliH",256))),
-(1011 , 'OscarT' , AES_ENCRYPT('OscarPw',SHA2("OscarT",256)));
+(1011 , 'OscarT' , AES_ENCRYPT('OscarPw',SHA2("OscarT",256))),
+(1022 , 'JoeG' , AES_ENCRYPT('JoePw',SHA2("JoeG",256)));
 
 INSERT INTO `handler` VALUES
 (2001),
@@ -42,6 +43,7 @@ INSERT INTO `handler` VALUES
 (2004),
 (2009),
 (2010),
+(1022),
 (1011);
 
 INSERT INTO `hardware` 
@@ -101,22 +103,42 @@ INSERT INTO `analyst` VALUES
 (2007, 2007);
 
 INSERT INTO `ticket` (`ticket_id`, `employee_id`, `status`, `priority`, `problem_description`, `notes`, `creation_date`, `last_updated`, `handler_id`, `operating_system`, `hardware_id`, `software_id`, `problem_type_id`, `number_of_drops`, `closing_date`, `closing_time`) VALUES
-(1, 2005, 'active' , 'medium' ,'Computer not working' ,'C stuck on black screen when turning on' , '2022-03-09' , '2022-03-09' , 2002 ,  'Apple iOS 14', 1 ,  2 ,  2 , 1,  NULL , NULL ),
-(2, 2005, 'dropped','low' , 'Zoom meeting not connecting', 'Zoom connection unstable' , '2022-03-01', '2022-03-02' ,  2001, 'Microsoft Windows 11' ,  4, 1 ,    4 , 1 ,NULL ,NULL),
-(3, 2008, 'active' ,'low', 'Reboot system and replace cartridges', 'Canon Pixma not detecting ink cartridges '  ,'2022-03-01' , '2022-03-03', 1011, 'Microsoft Windows 11' , 3, 2,  7 , 0 ,'2022-03-03', '01:00:30'),
-(4, 2008, 'closed','high' , 'Boot OS from an external hard drive or USB' ,'Apple OS refuses to boot' , '2022-02-23', '2022-02-24' , 2010, 'Apple iOS 14' ,  5 ,2,  6 , 0 ,'2022-02-24','12:34:00'),
-(5, 2014, 'active' ,'medium' ,  'Credentials not working' ,'Department MS Office credentials not working' , '2022-03-01' ,'2022-03-01', 2001,'Microsoft Windows 10' ,  2,   2 ,  3 , 0 ,NULL , NULL    ),
-(6, 2005, 'submitted' , 'high' ,  'Excel not opening' , 'Screen just freezes' , '2022-03-01' ,'2022-03-01', 2001,'Microsoft Windows 10', 2, 2, 10, 0 , NULL , NULL    );    
+(1, 2005, 'active' , 'medium' ,'Computer not working' ,'C stuck on black screen when turning on' , '2022-03-09' , '2022-03-09' , 2002 ,  'Apple iOS 14', 1 ,  2,  2 , 1,  NULL , NULL ),
+(2, 2005, 'dropped','low' , 'Zoom meeting not connecting', 'Zoom connection unstable' , '2022-03-01', '2022-03-02' ,  2001, 'Apple Mac OSX 12' ,  4, 8,    5 , 1 ,NULL ,NULL),
+(3, 2008, 'active' ,'low', 'Reboot system and replace cartridges', 'Canon Pixma not detecting ink cartridges '  ,'2022-03-01' , '2022-03-03', 1011, 'Microsoft Windows 11' , 3, 7,  7, 0 ,'2022-03-03', '01:00:30'),
+(4, 2008, 'closed','high' , 'Boot OS from an external hard drive or USB' ,'Apple OS refuses to boot' , '2022-02-23', '2022-02-24' , 2010, 'Apple iOS 14' ,  5 , 6,  9, 0 ,'2022-02-24','12:34:00'),
+(5, 2014, 'active' ,'medium' ,  'Credentials not working' ,'Department MS Office credentials not working' , '2022-03-01' ,'2022-03-01', 2001,'Apple Mac OSX 12' ,  2,   1 ,   12, 0 ,NULL , NULL    ),
+(6, 2005, 'submitted' , 'high' ,  'Excel not opening' , 'Screen just freezes' , '2022-03-01' ,'2022-03-01', 2001,'Microsoft Windows 10', 2, 2, 10, 0 , NULL , NULL    ),
+(7, 2005, 'active' , 'medium' ,'Computer not working' ,'C stuck on black screen when turning on' , '2022-03-09' , '2022-03-09' , 2002 ,  'Apple iOS 15', 1 ,  5 ,  11, 1,  NULL , NULL ),
+(8, 2005, 'dropped','low' , 'Zoom meeting not connecting', 'Zoom connection unstable' , '2022-03-01', '2022-03-02' ,  2001, 'Microsoft Windows 11' ,  1, 7 , 8, 1 ,NULL ,NULL),
+(9, 2008, 'active' ,'low', 'Reboot system and replace cartridges', 'Canon Pixma not detecting ink cartridges '  ,'2022-03-01' , '2022-03-03', 1011, 'Microsoft Windows 11' , 7, 7,  3, 0 ,'2022-03-03', '01:00:30'),
+(10, 2008, 'closed','high' , 'Boot OS from an external hard drive or USB' ,'Apple OS refuses to boot' , '2022-02-23', '2022-02-24' , 2010, 'Apple Mac OSX 12' ,  7 , 1,  5, 0 ,'2022-02-24','12:34:00'),
+(11, 2014, 'active' ,'medium' ,  'Credentials not working' ,'Department MS Office credentials not working' , '2022-03-01' ,'2022-03-01', 2001,'Microsoft Windows 10' ,  2,   23,  9, 0 ,NULL , NULL    ),
+(12, 2005, 'submitted' , 'medium' ,'Computer not working' ,'C stuck on black screen when turning on' , '2022-03-09' , '2022-03-09' , 2002 ,  'Apple iOS 15', 1 ,  6 ,  10, 1,  NULL , NULL ),
+(13, 2005, 'dropped','low' , 'Zoom meeting not connecting', 'Zoom connection unstable' , '2022-03-01', '2022-03-02' ,  2001, 'Apple Mac OSX 12' , 4, 5 ,    12, 1 ,NULL ,NULL),
+(14, 2008, 'submitted' ,'low', 'Reboot system and replace cartridges', 'Canon Pixma not detecting ink cartridges '  ,'2022-03-01' , '2022-03-03', 1011, 'Microsoft Windows 11' , 3, 8,  1, 0 ,'2022-03-03', '01:00:30'),
+(15, 2008, 'closed','high' , 'Boot OS from an external hard drive or USB' ,'Apple OS refuses to boot' , '2022-02-23', '2022-02-24' , 1022, 'Apple iOS 14' ,  5 ,7,  6, 0 ,'2022-02-24','12:34:00'),
+(16, 2014, 'unsuccessful' ,'medium' ,  'Credentials not working' ,'Department MS Office credentials not working' , '2022-03-01' ,'2022-03-01', 2001,'Apple Mac OSX 12' ,  2,   2 ,  7, 0 ,NULL , NULL    );    
 
 
 INSERT INTO `solution` VALUES
 (1, "Reboot system and replace cartridges"),  
-(2, "Boot OS from an external hard drive or USB");
+(2, "Boot OS from an external hard drive or USB"),
+(3, "Reboot system and replace cartridges"),  
+(4, "Boot OS from an external hard drive or USB"),  
+(5, "Reboot system and replace cartridges"),  
+(6, "Boot OS from an external hard drive or USB"),  
+(7, "Reboot system and replace cartridges"),;
 
 
 INSERT INTO `ticket_solution`  VALUES
-(4 , 1, 'successful' ,2010),
-(6 , 2, 'pending' ,2001);
+(4 , 1, 'successful', 2010),
+(6 , 2, 'pending', 2001),
+(10 , 3, 'successful', 2010),
+(12 , 4, 'pending', 2002),
+(14 , 5, 'unsuccessful', 1011),
+(14 , 6, 'pending', 1011),
+(16 , 7, 'unsuccessful', 2001);
 
 
 INSERT INTO `skillset` (`skill_id`, `handler_id`, `problem_type_id`) VALUES
@@ -139,7 +161,9 @@ INSERT INTO `skillset` (`skill_id`, `handler_id`, `problem_type_id`) VALUES
 
 INSERT INTO `dropped`  VALUES
 (1, 'Employee ran Windows network diagnostics - specialist not needed', '2022-03-02', '15:25:03', 1, 2002),
-(2, 'Solution is not in my area of expertise', '2022-03-02', '15:25:03', 2, 2001);
+(2, 'Solution is not in my area of expertise', '2022-03-02', '15:25:03', 8, 2001),
+(3, 'Employee ran Windows network diagnostics - specialist not needed', '2022-03-02', '15:25:03', 13, 2001),
+(4, 'Solution is not in my area of expertise', '2022-03-02', '15:25:03', 2, 2001);
 
 
 INSERT INTO history_log(ticket_id, handler_id, edited_item, new_value, date_time)
