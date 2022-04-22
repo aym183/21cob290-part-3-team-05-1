33ee31a86d69ee834d64d24fe269c1b2cfbc9275
Ajmal Abdul Majeed <ajough18@gmail.com>
2022-04-22 16:45:51 +0100

    Update server.js

--- project/server.js
+++ project/server.js
@@ -546,8 +546,6 @@ app.get('/intspecialist.html', (req, res) => {
             // console.log(msg.current_date);
             // console.log(msg.id);
             // console.log(session_id);
-            console.log(msg.changed_names[0]);
-            console.log(msg.changed_values[0]);
             con.query(`UPDATE ticket
             SET status = 'dropped' WHERE ticket_id = ?`,[msg.id], function (err, result, fields){
                 if (err) throw err;
@@ -561,21 +559,21 @@ app.get('/intspecialist.html', (req, res) => {
 
 })
 
-// io.on('connection', (socket) => {
-//     console.log('connected')
+io.on('connection', (socket) => {
+    console.log('connected')
     
 
-//     socket.on('update_history', (msg) => {
+    socket.on('update_history', (msg) => {
 
-//         con.query(`INSERT into history_log (ticket_id, handler_id, edited_item, new_value, date_time)
-//                     values(?, ?, ?, ?, ?)`, [msg.id, parseInt(session_id), msg.changed_names[0], msg.changed_values[0], msg.current_dateTime], function (err, result, fields){
+        con.query(`INSERT into history_log (ticket_id, handler_id, edited_item, new_value, date_time)
+                    values(?, ?, ?, ?, ?)`, [msg.id, parseInt(session_id), msg.changed_names[0], msg.changed_values[0], msg.current_dateTime], function (err, result, fields){
         
-//                         if (err) throw err;
-//                     });
+                        if (err) throw err;
+                    });
 
-//     })
+    })
 
-// });
+});
 
 } else {
     res.redirect('/login.html');
