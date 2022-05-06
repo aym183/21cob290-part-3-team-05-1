8e3087f455f4598eaf1c2ba26e5f2fca8f6e546a
Ajmal Abdul Majeed <ajough18@gmail.com>
2022-05-06 07:37:13 +0100

    Added unsolvable code to server side
    Code for internal specialist

--- project/server.js
+++ project/server.js
@@ -583,15 +583,31 @@ app.get('/intspecialist.html', (req, res) => {
                 no_of_drops = result[0].number_of_drops;
                 new_no_drops = parseInt(no_of_drops)+1;
 
-            con.query(`UPDATE ticket
-            SET status = 'dropped', number_of_drops = ? WHERE ticket_id = ?`,[new_no_drops, msg.id], function (err, result, fields){
-                if (err) throw err;
+                if(new_no_drops == 5){
+                    con.query(`UPDATE ticket
+                    SET status = 'unsolvable', number_of_drops = ? WHERE ticket_id = ?`,[new_no_drops, parseInt(msg.id)], function (err, result, fields){
+                    if (err) throw err;
 
-            con.query(`INSERT into dropped (reason, drop_date, drop_time, ticket_id, handler_id)
-            values(?, ?, ?, ?, ?)`, [msg.reason, msg.current_date, msg.current_dateTime, msg.id, session_id], function (err, result, fields){
+                    con.query(`INSERT into dropped (reason, drop_date, drop_time, ticket_id, handler_id)
+                    values(?, ?, ?, ?, ?)`, [msg.reason, msg.current_date, msg.current_dateTime, parseInt(msg.id), parseInt(session_id)], function (err, result, fields){
+        
+                        if (err) throw err;
+                    });
+                })
+                }
+                else{
 
-            });
-            });
+                    con.query(`UPDATE ticket
+                    SET status = 'dropped', number_of_drops = ? WHERE ticket_id = ?`,[new_no_drops, parseInt(msg.id)], function (err, result, fields){
+                    if (err) throw err;
+
+                    con.query(`INSERT into dropped (reason, drop_date, drop_time, ticket_id, handler_id)
+                    values(?, ?, ?, ?, ?)`, [msg.reason, msg.current_date, msg.current_dateTime, parseInt(msg.id), parseInt(session_id)], function (err, result, fields){
+        
+                        if (err) throw err;
+                    });
+                })
+                }
         });
         })
 })
